3424452da3b39bfc8f2523cd98c3d922323cf61d
stephane <stephanesome@gmail.com>
2021-08-29 13:15:36 -0400

    Final

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,14 +2,26 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { HeaderComponent } from './header/header.component';
+import { AddressListComponent } from './address-list/address-list.component';
+import { AddressListElementComponent } from './address-list/address-list-element/address-list-element.component';
+import { AddressViewComponent } from './address-list/address-view/address-view.component';
+import {FormsModule} from "@angular/forms";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    AddressListComponent,
+    AddressListElementComponent,
+    AddressViewComponent
   ],
-  imports: [
-    BrowserModule
-  ],
+    imports: [
+        BrowserModule,
+        NgbModule,
+        FormsModule
+    ],
   providers: [],
   bootstrap: [AppComponent]
 })
